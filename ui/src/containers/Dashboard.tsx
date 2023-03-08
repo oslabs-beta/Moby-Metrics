34c4eac07ebabb3d364dfcf5bf3a765d6983f82c
Jason Brown <superbunker@gmail.com>
2023-03-08 14:50:10 -0800

    charts are working

--- ui/src/containers/Dashboard.tsx
+++ ui/src/containers/Dashboard.tsx
@@ -2,6 +2,88 @@ import React from 'react';
 import { Box, Stack, TextField, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
+
+export const barOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Very Important Container Data',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const barData = {
+  labels,
+  datasets: [
+    {
+      label: 'Important Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Important Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export const doughData = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
 type Props = {};
 
 const client = createDockerDesktopClient();
@@ -37,13 +119,16 @@ const Dashboard = (props: Props) => {
       setContainers(result.parseJsonLines());
     });
   }
-    
-  
+
+
 
   return (
     <Box>
       {/* // <Typography variant='h3'>Docker extension demo</Typography>
       //{' '} */}
+
+      <Bar options={barOptions} data={barData} />
+
       <Typography
         variant='body1'
         color='text.secondary'
@@ -80,6 +165,9 @@ const Dashboard = (props: Props) => {
           minRows={5}
           value={JSON.stringify(containers, undefined, 2) ?? ''}/>
       </Stack>
+
+      <Doughnut data={doughData} />;
+
     </Box>
   );
 };
